2f58c39e6aa86fd533f8d030d937bfe969b199c6
AConFarbaseManuel <84882363+AConFarbaseManuel@users.noreply.github.com>
2021-05-26 20:00:33 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyDuImL2B-dUpzeCrAtqQaFJgZXCfxDPhfQ",
     authDomain: "aconfarbasemanuel-47117.firebaseapp.com",
     projectId: "aconfarbasemanuel-47117",
@@ -6,7 +6,5 @@ var firebaseConfig = {
     messagingSenderId: "259690719436",
     appId: "1:259690719436:web:cc1291b9356f81442ffa69",
     measurementId: "G-MLVQBEF6RK"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  });
+ 
