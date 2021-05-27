ded2755b06eac4959ea8db9748b557963e4ff7d1
AConFarbaseManuel <84882363+AConFarbaseManuel@users.noreply.github.com>
2021-05-26 19:29:57 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDuImL2B-dUpzeCrAtqQaFJgZXCfxDPhfQ",
+    authDomain: "aconfarbasemanuel-47117.firebaseapp.com",
+    projectId: "aconfarbasemanuel-47117",
+    storageBucket: "aconfarbasemanuel-47117.appspot.com",
+    messagingSenderId: "259690719436",
+    appId: "1:259690719436:web:cc1291b9356f81442ffa69",
+    measurementId: "G-MLVQBEF6RK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
